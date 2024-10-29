7f1ddfb489cb10a40ffba9ef1eda49a26db122ae
Elizabeth Awosanya <e.e.awosanya@wlv.ac.uk>
2024-10-29 13:45:33 +0000

    feat: update entry route file

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import Header from './components/Header';
 import ProductsList from './pages/Products/ProductsList';
 import ProductDetails from './pages/Products/ProductDetails';
 import ProductEdit from './pages/Products/ProductEdit';
+import OrdersList from './pages/Orders/OrdersList';
+import OrderDetails from './pages/Orders/OrderDetails';
+import OrderEdit from './pages/Orders/OrderEdit';
+import UsersList from './pages/Users/UsersList';
+import UserDetails from './pages/Users/UserDetails';
+import UserEdit from './pages/Users/UserEdit';
 
 const App = () => (
   <Router>
     <Header />
     <Routes>
+      {/* Product Routes */}
       <Route path="/products" element={<ProductsList />} />
       <Route path="/products/:id" element={<ProductDetails />} />
       <Route path="/products/:id/edit" element={<ProductEdit />} />
-      {/* Add similar routes for Orders and Users */}
+
+      {/* Order Routes */}
+      <Route path="/orders" element={<OrdersList />} />
+      <Route path="/orders/:id" element={<OrderDetails />} />
+      <Route path="/orders/:id/edit" element={<OrderEdit />} />
+
+      {/* User Routes */}
+      <Route path="/users" element={<UsersList />} />
+      <Route path="/users/:id" element={<UserDetails />} />
+      <Route path="/users/:id/edit" element={<UserEdit />} />
     </Routes>
   </Router>
 );
